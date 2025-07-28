2b761d7c3dad434c10ea12c0348d0655846f0098
Adithya Firmansyah Putra <aditputrafirmansyah@gmail.com>
2025-07-29 00:09:27 +0700

    initial commit

---
+++ Project.swift
@@ -0,0 +1,54 @@
+import ProjectDescription
+
+let baseAppBundleId: String = Environment.bundleId.getString(default: "com.ada.il")
+let appName: String = "spwit"
+
+func bundleId(for target: String) -> String {
+    return "\(baseAppBundleId).\(target)"
+}
+
+let project = Project(
+    name: appName,
+    targets: [
+        .target(
+            name: appName,
+            destinations: .iOS,
+            product: .app,
+            bundleId: bundleId(for: appName),
+            infoPlist: .extendingDefault(
+                with: [
+                    // "UILaunchStoryboardName": "LaunchScreen",
+                    "UIApplicationSceneManifest": [
+                        "UIApplicationSupportsMultipleScenes": false,
+                        "UISceneConfigurations": [
+                            "UIWindowSceneSessionRoleApplication": [
+                                [
+                                    "UISceneConfigurationName": "Default Configuration",
+                                    "UISceneDelegateClassName":
+                                        "$(PRODUCT_MODULE_NAME).SceneDelegate",
+                                        // "UISceneStoryboardFile": "Main"
+                                ]
+                            ]
+                        ],
+                    ]
+                ]
+            ),
+            sources: ["spwit/Sources/**"],
+            resources: ["spwit/Resources/**"],
+            // "spwit/Sources/**/*.storyboard", "spwit/Sources/**/*.xib",
+            dependencies: [
+                .external(name: "Alamofire")
+            ]
+        ),
+        .target(
+            name: "\(appName)Tests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: bundleId(for: "\(appName)Tests"),
+            infoPlist: .default,
+            sources: ["spwit/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: appName)]
+        ),
+    ]
+)
