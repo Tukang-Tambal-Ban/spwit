4472d60119a85f17c95ea4f06b5c8f4ff662e979
Itsqy <syatriarifqi@gmail.com>
2025-08-01 14:36:48 +0700

    feat : slicing sign screen

--- spwit/Sources/Presentation/Auth/SignInView/SignInViewController.swift
+++ spwit/Sources/Presentation/Auth/SignInView/SignInViewController.swift
@@ -13,6 +13,8 @@ class SignInViewController: UIViewController, SignInViewProtocol {
 
     private let signInButton = UIButton(type: .system)
     private let resultLabel = UILabel()
+    private let ctaLabel = UILabel()
+    private let logoApp =  UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,24 +22,64 @@ class SignInViewController: UIViewController, SignInViewProtocol {
     }
 
     private func setupUI() {
-        view.backgroundColor = .systemBackground
-
-        // Setup Button
-        signInButton.setTitle("Sign in with Apple", for: .normal)
+        
+        view.backgroundColor = UIColor(hex: "#0F213C")
+        
+        logoApp.image = UIImage(named: "spwit_logo")
+        logoApp.contentMode = .scaleAspectFit
+        logoApp.translatesAutoresizingMaskIntoConstraints = false
+        
+        ctaLabel.text = "Let's get started"
+        ctaLabel.textColor = UIColor(hex: "A4F000")
+        ctaLabel.textAlignment = .center
+        ctaLabel.numberOfLines = 0
+        ctaLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+//        sign in Button
+        let appleIcon = UIImage(systemName: "apple.logo")
+        signInButton.setImage(appleIcon, for: .normal)
+        signInButton.tintColor = .black // or your preferred color
+        signInButton.setTitle(" Sign In With Apple", for: .normal)
+        signInButton.setTitleColor(.black, for: .normal) // <-- Use black or your desired color
+        signInButton.backgroundColor = .white
+        signInButton.layer.borderColor = UIColor.black.cgColor
+        signInButton.layer.cornerRadius = 24
+        signInButton.clipsToBounds = true
         signInButton.addTarget(self, action: #selector(signInTapped), for: .touchUpInside)
         signInButton.translatesAutoresizingMaskIntoConstraints = false
 
+
+ 
+        
         // Setup Result Label
         resultLabel.textAlignment = .center
         resultLabel.numberOfLines = 0
         resultLabel.translatesAutoresizingMaskIntoConstraints = false
 
+        view.addSubview(ctaLabel)
         view.addSubview(signInButton)
         view.addSubview(resultLabel)
+        view.addSubview(logoApp)
+       
 
         NSLayoutConstraint.activate([
+            
+            logoApp.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoApp.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -103),
+            logoApp.widthAnchor.constraint(equalToConstant: 279),
+            logoApp.heightAnchor.constraint(equalToConstant: 120),
+            
+            ctaLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            ctaLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            ctaLabel.bottomAnchor.constraint(equalTo: signInButton.topAnchor,constant: -20),
+            
+            signInButton.widthAnchor.constraint(equalToConstant: 360),   // width: 200 pts
+            signInButton.heightAnchor.constraint(equalToConstant: 48) ,
+            
+            signInButton.widthAnchor.constraint(equalToConstant: 360),   // width: 200 pts
+            signInButton.heightAnchor.constraint(equalToConstant: 50) ,   // height: 48 pts
             signInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            signInButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            signInButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -237),
 
             resultLabel.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 20),
             resultLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
@@ -72,4 +114,25 @@ class SignInViewController: UIViewController, SignInViewProtocol {
     func hideLoading() {
         // You could reset or do nothing
     }
+    
 }
+
+
+#if DEBUG
+import SwiftUI
+
+struct SignInViewControllerPreview: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> SignInViewController {
+        return SignInViewController()
+    }
+    func updateUIViewController(_ uiViewController: SignInViewController, context: Context) {}
+}
+
+struct SignInViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInViewControllerPreview()
+            .edgesIgnoringSafeArea(.all)
+            .previewDisplayName("UIKit: Sign In Screen")
+    }
+}
+#endif
---
+++ spwit/Sources/Untitled.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  spwit
+//
+//  Created by Muhammad Rifqi Syatria on 8/1/25.
+//
+
