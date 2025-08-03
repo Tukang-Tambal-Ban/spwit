d068a7f0ec0b1b0e41505336aee727323f2574c5
Adithya Firmansyah Putra <aditputrafirmansyah@gmail.com>
2025-08-03 22:28:43 +0700

    fix: rename and reesolve fix

---
+++ spwit/Sources/Presentation/Expenses/AssignExpenses/ExpenseItem.swift
@@ -0,0 +1,26 @@
+//
+//  ExpenseItem.swift
+//  Spwit
+//
+//  Created by Adithya Firmansyah Putra on 03/08/25.
+//
+
+
+//
+//  ExpenseItem.swift
+//  Spwit
+//
+//  Created by Muhammad Rifqi Syatria on 8/3/25.
+//
+
+
+struct ExpenseItem {
+    var name: String
+    var amount: String
+    var checked: Bool
+}
+var expenseList: [ExpenseItem] = [
+    ExpenseItem(name: "You", amount: "Rp750.000", checked: true),
+    ExpenseItem(name: "Adit Lucu", amount: "Rp750.000", checked: true),
+    ExpenseItem(name: "Raiza Cantik", amount: "Rp0", checked: true)
+]
